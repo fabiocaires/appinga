bf099de7f64c4a4d44fb45bb815310b67a50ce25
fabiocaires <fabiocaires@gmail.com>
2015-02-02 02:01:48 -0200

    Calculo de ranking
    Agora as bebidas aparecem listadas na ordem da mais barata para a mais
    cara.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,50 @@
 $(function(){
 
-	function calcularBebidas(){
-		$("#ranking .ui-content ol").empty();
-		$("#entrada .ui-content input").each(function(){
-			nomeBebida = $("label[for='"+$(this).attr('id')+"']").text();
-			precoBebida = parseFloat($(this).val().replace(',','.'));
-			qtdBebida = $(this).attr("ml");
-			teorAlcool = $(this).attr("grau") / 100;
-			mlAlcool = teorAlcool*qtdBebida;
-			razaoAlcool = (precoBebida/mlAlcool).toFixed(2);
-			razaoAlcool = razaoAlcool.toString().replace('.',',');
-			$("#ranking .ui-content ol").append("<li>"+nomeBebida+" R$ "+razaoAlcool+"</li>");
+	function countDrinks(){
+		var ranking = [];
+
+		$("#ranking .ui-content ol")
+			.empty();
+
+		$("#entrada .ui-content input")
+			.each(function(){
+				if($(this).val()){
+					var drinkName 	= $("label[for='"+$(this)
+										.attr('id')+"']")
+										.text();
+
+					var drinkPrice 	= parseFloat($(this)
+										.val()
+										.replace(',','.'));
+
+					var drinkAmount = $(this)
+										.attr("ml");
+
+					var alcoholic 	= $(this)
+										.attr("grau") / 100;
+
+					var density 	= alcoholic*drinkAmount;
+
+					var reason 		= ((drinkAmount/density)
+											.toFixed(2));
+
+					ranking.push({ name : drinkName, price : drinkPrice }); 
+				}
 		});
+		buildRanking(ranking);
+	}
+
+	function buildRanking(ranking){
+		ranking = ranking.sort(function(a,b) { return parseFloat(a.price) - parseFloat(b.price)});
+		for(drink in ranking){
+			var name = ranking[drink].name.toString().replace('.',',');
+			var price = ranking[drink].price.toString().replace('.',',');
+			$("#ranking .ui-content ol").append("<li>"+name+" R$ "+price+"</li>");
+		}
 	}
 
 	$("#btn-calcular").click(function(){
-		calcularBebidas();
+		countDrinks();
 	});
+
 });
