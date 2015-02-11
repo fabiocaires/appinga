a3551213644925e5a50789480dc4cd9ac5d5d17c
fabiocaires <fabiocaires@gmail.com>
2015-02-11 07:50:45 -0200

    Conversão de jQuery pra Zepto

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,51 @@ $(function(){
 	function countDrinks(){
 		var ranking = [];
 
-		$("#ranking .ui-content ol")
-			.empty();
+		// $("#ranking .ui-content ol")
+		// 	.empty();
+		var elements = $("#input-prices input");
 
-		$("#entrada .ui-content input")
-			.each(function(){
-				if($(this).val()){
-					var drinkName 	= $("label[for='"+$(this)
-										.attr('id')+"']")
-										.text();
+		for( i = 0; i < elements.length; i++ ){
+			if(elements[i].value != ""){
+				var element 		= $(elements[i]);
+				var drinkName 		= element.prop("title");
+				var drinkPrice		= parseFloat(element.value);
+				var drinkPrice 		= drinkPrice.replace("," , ".");
+				var drinkAmount		= element.prop("ml");
+				var drinkAlcohol 	= element.prop("grau");
+				var drinkDensity	= drinkAlcohol*drinkAmount;
+				var drinkReason		= ((drinkPrice/drinkDensity).toFixed(2));
+				ranking.push({ name : drinkName, price : drinkReason});
+			}
+		}
+		console.log(ranking);
+		//console.log(values);
+		// $("#input-prices input")
+		// 	.map(function(){
+		// 		if($(this).val()){
+		// 			var drinkName 	= $("label[for='"+$(this)
+		// 								.attr('id')+"']")
+		// 								.text();
 
-					var drinkPrice 	= parseFloat($(this)
-										.val()
-										.replace(',','.'));
+		// 			var drinkPrice 	= parseFloat($(this)
+		// 								.val()
+		// 								.replace(',','.'));
 
-					var drinkAmount = $(this)
-										.attr("ml");
+		// 			var drinkAmount = $(this)
+		// 								.attr("ml");
 
-					var alcoholic 	= $(this)
-										.attr("grau") / 100;
+		// 			var alcoholic 	= $(this)
+		// 								.attr("grau") / 100;
 
-					var density 	= alcoholic*drinkAmount;
+		// 			var density 	= alcoholic*drinkAmount;
 
-					var reason 		= ((drinkPrice/density)
-										.toFixed(2));
+		// 			var reason 		= ((drinkPrice/density)
+		// 								.toFixed(2));
 
-					ranking.push({ name : drinkName, price : reason }); 
-				}
-		});
-		buildRanking(ranking);
+		// 			ranking.push({ name : drinkName, price : reason }); 
+		// 		}
+		// });
+		// buildRanking(ranking);
 	}
 
 	function buildRanking(ranking){
@@ -43,7 +59,8 @@ $(function(){
 		}
 	}
 
-	$("#btn-calcular").click(function(){
+	$("#btn-ranking").on('click', function(e){
+		e.preventDefault();
 		countDrinks();
 	});
 
