946cb29f95cd993b17c714cb1692e68f6b934a48
fabiocaires <fabiocaires@gmail.com>
2015-02-14 20:05:25 -0200

    Conversão para JS Puro
    Conversão do código para JS puro para depois adaptar para o angular.js,
    remoção do zepto para a adição do hammer.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,62 @@
-$(function(){
+(function(){
 
-	function countDrinks(){
+	document.addEventListener("DOMContentLoaded", function() { 
 
-		var ranking = [];
-		var elements = $("#input-prices input");
+		function countDrinks(event){
+			event.preventDefault();
+			var ranking = [];
+			var elements = document.querySelectorAll("#input-prices input");
 
-		$("#page-2 ol").empty();
 
-		for( i = 0; i < elements.length; i++ ){
-			if(elements[i].value != ""){
-				var element 		= $(elements[i]);
-				var drinkName 		= element.prop("title");
-				var drinkPrice		= element.val();
-					drinkPrice 		= drinkPrice.replace("," , ".");
-				var drinkAmount		= element.data("ml");
-				var drinkAlcohol 	= element.data("grau") / 100;
-				var drinkDensity	= drinkAlcohol*drinkAmount;
-				var drinkReason		= ((drinkPrice/drinkDensity).toFixed(2));
-
-				ranking.push({ name : drinkName, price : drinkReason});
+			var rankingResult = document.querySelectorAll("#page-2 ol li");
+			
+			for( i = 0; i < rankingResult.length; i++ ){
+				rankingResult[i].remove(i);
 			}
+
+			// precisa arrumar essa parte, a leitura dos atributos ml, grau
+			// não retornam números, mesmo que rode um parseInt() a conta não funcioca
+
+			for( i = 0; i < elements.length; i++ ){
+				if(elements[i].value != ""){
+					var element 		= elements[i];
+					var drinkName 		= element.getAttribute("title");
+					var drinkPrice		= element.value;
+						drinkPrice 		= drinkPrice.replace("," , ".");
+					var drinkAmount		= element.getAttribute("ml");
+					var drinkAlcohol 	= element.getAttribute("grau")) / 100;
+					var drinkDensity	= drinkAlcohol*drinkAmount;
+					console.log(drinkAmount+" "+drinkAlcohol+" "+drinkDensity);
+					var drinkReason		= ((drinkPrice/drinkDensity).toFixed(2));
+
+					ranking.push({ name : drinkName, price : drinkReason});
+				}
+			}
+			console.log(ranking);
+			// btnRanking(ranking);
 		}
 
-		buildRanking(ranking);
-	}
+		// function buildRanking(ranking){
 
-	function buildRanking(ranking){
+		// 	ranking = ranking.sort(function(a,b) { return parseFloat(a.price) - parseFloat(b.price)});
 
-		ranking = ranking.sort(function(a,b) { return parseFloat(a.price) - parseFloat(b.price)});
+		// 	for(drink in ranking){
+		// 		var name = ranking[drink].name.toString().replace('.',',');
+		// 		var price = ranking[drink].price.toString().replace('.',',');
+		// 		var list = document.querySelector("#page-2 ol");
+		// 		var elementList = document.createElement("li");
+		// 		var contentElementList = document.createTextNode(name+" R$ "+price);
+		// 		elementList.appendChild(contentElementList);
+		// 		list.appendChild(elementList);
+		// 	}
+		// 	var page1 = document.getElementById("#page-1");
+		// 	var page2 = document.getElementById("#page-2");
+		// 	page1.style.display = "none";
+		// 	page2.style.display = "block";
+		// }
 
-		for(drink in ranking){
-			var name = ranking[drink].name.toString().replace('.',',');
-			var price = ranking[drink].price.toString().replace('.',',');
-			$("#page-2 ol").append("<li>"+name+" R$ "+price+"</li>");
-			$("#page-1").hide();
-			$("#page-2").show();
-		}
-	}
-
-	$("#btn-ranking").on('click', function(e){
-		e.preventDefault();
-		countDrinks();
+		var btnRanking = document.getElementById("btn-ranking");
+		btnRanking.addEventListener("click", countDrinks, false);
 	});
 
-});
+})();
